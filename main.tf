63067215bff445fc93d5f317d5b41033dde78a0f
dlavric <daniela@hashicorp.com>
2022-01-13 14:48:58 +0100

    added files

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "random" {
+  # Configuration options
+}
+
+resource "random_id" "server" {
+  prefix = "Daniela"
+
+  byte_length = 8
+}
+
+output "server" {
+  value = random_id.server.prefix
+}
